1362b0ea913506531cf54c7b846c0af24d054967
Ryosuke Tachi <Tachi@ip253-250.wlan.kobe-u.ac.jp>
2017-06-16 10:49:43 +0900

    Initial commit

--- FINAL_TASK/w12_main_ex02.js
+++ FINAL_TASK/w12_main_ex02.js
@@ -1,3 +1,20 @@
+var isovalue = 128;
+var volume = new KVS.LobsterData();
+var screen = new KVS.THREEScreen();
+var surfaces = Isosurfaces( volume, isovalue );
+var target = document.getElementById("change-isovalue-button");
+var slider = document.getElementById("isovalue");
+
+function OnButtonClick() {
+  screen.scene.remove(surfaces);
+  surfaces = Isosurfaces( volume, slider.value );
+  screen.scene.add( surfaces );
+}
+slider.addEventListener("input", function() {
+// ドラッグ中のイベント
+  target.innerHTML = "isovalue="+slider.value;
+}, false);
+
 function main()
 {
     screen.init( volume, {
